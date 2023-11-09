edfd647b3e127cccdb9fb6bef5ece281777c225f
Gaston Solari Loudet <gsolaril@itba.edu.ar>
2023-11-09 14:06:21 -0300

    "DebianDownloader" gets the "directory" dict with available files in mirror

--- core/base.py
+++ core/base.py
@@ -15,11 +15,27 @@ class DebianDownloader:
     REGEX_HREF = "(?<=href=\")[^/]+(?=\">)" # Should only include files, not subpaths (/).
     REGEX_DCNT = "(?<= )[0-9]+(?=\r)"       # Should only include numbers next to carry char.
     
-    #▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-    def __init__(self, filename: str):
+    #▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+    def __init__(self):
 
-        self.filename = filename
-        self.url = self.URL_BASE.format(filename = filename)
+        self._directory = self._get_directory()
+
+    #▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+    @classmethod
+    def _get_directory(cls):
+        """[PRIVATE] Get file directory with download count."""
+        url = cls.URL_BASE.format(filename = "")        # Use URL without endpoint to get directory page.
+        resp = requests.get(url = url, timeout = 30)    # HTTP request for directory page content.
+        resp = resp.content.decode("utf-8")             # Decode and convert binary content to string.
+        files = re.findall(cls.REGEX_HREF, resp)        # Extract filenames from "href" tags in the HTML.
+        dcount = re.findall(cls.REGEX_DCNT, resp)       # Extract download count; rightmost number in each line.
+        return dict(zip(files, map(int, dcount)))       # Zip both lists as dictionary. Numbers shall be ints.
+    
+    #▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+    @property
+    def directory(self):
+        """Returns a dict with available filenames as keys and download counts as values."""
+        return self._directory.copy()
     
 #█████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████
 #██████████████████████████████████████████████████████████████████████████████████████████████████████   Quick test   ███
@@ -27,6 +43,5 @@ class DebianDownloader:
 
 if (__name__ == "__main__"):
 
-    obj = DebianDownloader(filename = "Contents-amd64")
-    print("Filename:", obj.filename)
-    print("URL:", obj.url)
+    obj = DebianDownloader()
+    print("Directory:\n", obj.directory)
