24425b83016f64e96e0175db327f8a19b64ea622
Gaston Solari Loudet <gsolaril@itba.edu.ar>
2023-11-09 14:38:03 -0300

    "DebianDownloader.download" method gets the specified file from the mirror. Decompresses when file is "gzip"

--- core/base.py
+++ core/base.py
@@ -1,4 +1,4 @@
-import os, sys, re, requests
+import sys, re, requests
 from gzip import GzipFile
 from io import BytesIO
 
@@ -10,6 +10,15 @@ from utils.constants import *
 #█████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████
 
 class DebianDownloader:
+    """
+    Base class for content indexer of Debian packages. Instantiate and use:
+     - "`directory`" property to get the available files with download count.
+     - "`download`" method to specify and download files.\n
+    For more info visit:
+     - Help and definitions: "https://wiki.debian.org/RepositoryFormat#A.22Contents"
+     - Mirror page with directory: "http://ftp.uk.debian.org/debian/dists/stable/main/"
+    """
+    class FileNotFound(Exception): pass
 
     URL_BASE = "http://ftp.uk.debian.org/debian/dists/stable/main/{filename}"
     REGEX_HREF = "(?<=href=\")[^/]+(?=\">)" # Should only include files, not subpaths (/).
@@ -37,6 +46,45 @@ class DebianDownloader:
         """Returns a dict with available filenames as keys and download counts as values."""
         return self._directory.copy()
     
+    #▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+    def _check_exist_file(self, filename: str):
+        """[PRIVATE] Verify if specified file exists."""
+        return (filename in self._directory.keys())
+    
+    #▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+    def download(self, filename: str, path_save: str = None):
+        """
+        Download specified file from Debian repository.\n
+        Inputs:
+        - `filename` (`str`): The name of the file to download.\n
+        - `path_save` (`str`) (optional): Path to save file to.\n
+        Outputs:
+        - `content` (`str`): The content of the downloaded file.\n
+        """
+        # Check if file exists. Else raise error.
+        if not self._check_exist_file(filename):
+            raise self.FileNotFound("\"%s\"" % filename)
+        # If all good, HTTP request and download file.
+        url = self.URL_BASE.format(filename = filename)
+        resp = requests.get(url = url, timeout = 30)
+        content = resp.content
+
+        # If compressed, decompress before decoding.
+        if filename.endswith(".gz"):
+            content = BytesIO(content)
+            with GzipFile(fileobj = content) as file:
+                content = file.read()
+        # Convert content into string.
+        content = content.decode("utf-8")
+
+        # Store content if path is given.
+        if path_save is not None:
+            with open(path_save, "w", errors = "ignore") as file:
+                file.write(content) # Save content to path's file.
+                print(f"Saved \"{filename}\" to \"{path_save}\".")
+                
+        return content
+    
 #█████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████
 #██████████████████████████████████████████████████████████████████████████████████████████████████████   Quick test   ███
 #█████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████
@@ -44,4 +92,14 @@ class DebianDownloader:
 if (__name__ == "__main__"):
 
     obj = DebianDownloader()
-    print("Directory:\n", obj.directory)
+    filename = "Contents-amd64"
+    filename_gz = filename + ".gz"
+    filename_txt = "./temp/" + filename + ".txt"
+    print(SEPARATOR)
+    print("About to download \"%s\"" % filename_gz)
+    content = obj.download(filename_gz, filename_txt)
+
+    preview = content.splitlines()
+    print("\nPreview - first and last 5 lines:\n")
+    preview = [*preview[: 5], "...", *preview[-5 :]]
+    [print(line) for line in preview]; print(SEPARATOR)
--- utils/constants.py
+++ utils/constants.py
@@ -6,3 +6,5 @@ from platform import uname
 ARCH_LOCAL_MACHINE = uname().machine.lower()
 # Just the local path of wherever this repo is held.
 THIS_PATH = os.path.split(__file__)[0]
+
+SEPARATOR = "–" * 100
